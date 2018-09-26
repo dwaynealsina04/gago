d0b1d02f6e56398068643f3f77193ffe5c7a22e1
dwaynealsina04 <43573953+dwaynealsina04@users.noreply.github.com>
2018-09-26 22:42:36 +0800

    Add files via upload

---
+++ button.js
@@ -0,0 +1,150 @@
+function changeStyle1() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-ifg7vt6xfyp1.html";
+}
+
+function changeStyle2() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-k0trvqzfpzqc.html";
+}
+
+function changeStyle3() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-abxigzkkmcty.html";
+}
+
+function changeStyle4() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-u3s3dlcyrzvt.html";
+}
+
+function changeStyle5() {
+document.getElementById("deathnote").src="https://streamango.com/embed/nftrmknbqdctalsr/";
+}
+
+function changeStyle6() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-i12y6ptil7n4.html";
+}
+
+function changeStyle7() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-soeio7ktvrds.html";
+}
+
+function changeStyle8() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-bpw92xrl8i9w.html";
+}
+
+function changeStyle9() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-edho3fhujwq6.html";
+}
+
+function changeStyle10() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-3h6588vyq7zv.html";
+}
+
+function changeStyle11() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-lv3b9qnz8bg3.html";
+}
+
+function changeStyle12() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-q3rikd9718j9.html";
+}
+
+function changeStyle13() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-8q46abhw6gkx.html";
+}
+
+function changeStyle14() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-7nlif8rvdclt.html";
+}
+
+function changeStyle15() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-gk8qabd9isb8.html";
+}
+
+function changeStyle16() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-12pz5uuj46s3.html";
+}
+
+function changeStyle17() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-gdy3c0tgv2h3.html";
+}
+
+function changeStyle18() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-h8mvooxd6gml.html";
+}
+
+function changeStyle19() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-q4uwqp784168.html";
+}
+
+function changeStyle20() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-8mye7mij2kna.html";
+}
+
+function changeStyle21() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-m1v14prra485.html";
+}
+
+function changeStyle22() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-amuz2yl7vl9w.html";
+}
+
+function changeStyle23() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-aifj9dbeblda.html";
+}
+
+function changeStyle24() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-yd24hpk61n9a.html";
+}
+
+function changeStyle25() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-n2ovcc11emhj.html";
+}
+
+function changeStyle26() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-mkarn7rv0432.html";
+}
+
+function changeStyle27() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-cgz05nz3i43q.html";
+}
+
+function changeStyle28() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-f54kd0f2gwgq.html";
+}
+
+function changeStyle29() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-e999aeg91ya2.html";
+}
+
+function changeStyle30() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-3yw6fb23miop.html";
+}
+
+function changeStyle31() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-nh8exl0ps5o9.html";
+}
+
+function changeStyle32() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-53tw8otxmgy8.html";
+}
+
+function changeStyle33() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-53tw8otxmgy8.html";
+}
+
+function changeStyle34() {
+document.getElementById("deathnote").src="https://streamango.com/embed/spnlarkldtpkqtab/";
+}
+
+function changeStyle35() {
+document.getElementById("deathnote").src="https://www.mp4upload.com/embed-790kh7wjb6xl.html";
+}
+
+function changeStyle36() {
+document.getElementById("deathnote").src="https://streamango.com/embed/bprtpofprdsacncm/";
+}
+
+function changeStyle37() {
+document.getElementById("deathnote").src="https://streamango.com/embed/kktlpoapcbkmobct/";
+}
+
+
+
